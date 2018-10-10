590036320451507a1dd628f7d31b4d6f9a7b605f
Vlad Krupenko <dev.fixique@gmail.com>
2018-10-10 23:00:02 +0300

    update swift version to 4.2

--- MetalGlobe/Application/AppDelegate.swift
+++ MetalGlobe/Application/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     private var demoType: DemoType = .globe
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         setupDemo()
         window?.makeKeyAndVisible()
 
